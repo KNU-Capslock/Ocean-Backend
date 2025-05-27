af7946d2b44a584e56266380c724e2a4a9911c8f
MOJAN <sjs732@naver.com>
2025-05-28 04:36:17 +0900

    Fix: 경로 설정 에러 수정

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import users, clothes, posts
+from app.api import users, clothes, posts
 
 app = FastAPI(
     title="Ocean Backend",
