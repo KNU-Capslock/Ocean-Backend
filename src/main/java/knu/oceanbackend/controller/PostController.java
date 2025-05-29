8a17bc2768c0833e32395e5473475cd3502c7a66
YoonGangHoon <yghun021007@naver.com>
2025-05-29 21:17:18 +0900

    Docs: 게시물 엔드포인트마다 swagger 설명 추가

--- src/main/java/knu/oceanbackend/controller/PostController.java
+++ src/main/java/knu/oceanbackend/controller/PostController.java
@@ -1,5 +1,6 @@
 package knu.oceanbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import knu.oceanbackend.dto.post.PostRequestDto;
 import knu.oceanbackend.dto.post.PostResponseDto;
@@ -29,6 +30,10 @@ public class PostController{
 
     private final Path uploadDir = Paths.get("src/main/resources/static/posts");
 
+    @Operation(
+            summary = "게시물 생성",
+            description = "OOTD 사진을 넣으면 원본 저장 및 자동으로 AI 서버 전송 후 사진 저장"
+    )
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> createPost(
             HttpServletRequest request,
@@ -59,17 +64,29 @@ public class PostController{
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "게시물 단건 조회",
+            description = "게시물 id를 사용하여 단건 조회"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @Operation(
+            summary = "특정 사용자 게시물 조회",
+            description = "특정 사용자가 생성한 게시물 모두 조회(수정날짜 내림차순)"
+    )
     @GetMapping
     public ResponseEntity<List<PostResponseDto>> getPostsByUser(HttpServletRequest request) {
         Long userId = (Long) request.getAttribute("user_id");
         return ResponseEntity.ok(postService.getPostsByUser(userId));
     }
 
+    @Operation(
+            summary = "게시물 수정",
+            description = "title과 content 수정 가능, 이미지 수정 불가능"
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<Post> updatePost(
             @PathVariable Long id,
@@ -79,6 +96,10 @@ public class PostController{
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "게시물 삭제",
+            description = "게시물 id를 통해 삭제"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
